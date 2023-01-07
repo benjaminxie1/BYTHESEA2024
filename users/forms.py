b2b8fb8ff3781cba64f97254b9d6f3c6eade7e0a
benjaminxie1 <bxie2015@gmail.com>
2023-01-06 23:43:09 -0800

    migrations

--- users/forms.py
+++ users/forms.py
@@ -24,8 +24,7 @@ class ProfileForm(ModelForm):
         model = Profile
         fields = ['name', 'email', 'username',
                   'location', 'bio', 'short_intro', 'profile_image',
-                  'social_github', 'social_linkedin', 'social_twitter',
-                  'social_youtube', 'social_website']
+                  'dream_school', 'major']
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
--- users/models.py
+++ users/models.py
@@ -18,11 +18,8 @@ class Profile(models.Model):
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
         null=True, blank=True, upload_to='profiles/', default="profiles/user-default.png")
-    social_github = models.CharField(max_length=200, blank=True, null=True)
-    social_twitter = models.CharField(max_length=200, blank=True, null=True)
-    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
-    social_youtube = models.CharField(max_length=200, blank=True, null=True)
-    social_website = models.CharField(max_length=200, blank=True, null=True)
+    major = models.CharField(max_length=200, blank=True, null=True)
+    dream_school = models.CharField(max_length=200, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
